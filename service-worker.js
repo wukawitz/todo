27d1847f790937b8fc8ccffd5b4373bd06843ce0
wukawitz <wukawitz@gmail.com>
2025-03-09 17:04:59 -0400

    Add files via upload
    Initial commit.

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'todo-app-cache-v1';
+const urlsToCache = [
+  './index.html',
+  './todo.svg', // Changed from todo.png to match your icon
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
